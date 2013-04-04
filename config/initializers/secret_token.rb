4d3b18b38fde74272fcc9bbb0bc18a65a3641450
ntf <ntf@ntf-System-Product-Name.(none)>
2013-04-04 18:46:09 +0530

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'b132782330ef235a167899655f71b52d4c6263ebcbdd8e98f9d3197b1f0b2cf169bdb5d161cbcb2c06220d17ffb2b3c592a2ddc3821be23e7b29bbc33aff1c23'
