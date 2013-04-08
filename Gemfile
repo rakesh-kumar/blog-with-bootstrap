a691fb172796e776e0c36f800d85fb56c9d5410e
ntf <ntf@ntf-System-Product-Name.(none)>
2013-04-08 11:08:46 +0530

    article

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'rails', '3.2.13'
 gem 'sqlite3'
 gem 'therubyracer'
 gem 'json'
-
+gem 'devise' , '~>1.4.9'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404125507) do
+ActiveRecord::Schema.define(:version => 20130405112425) do
+
+  create_table "articles", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "name"
